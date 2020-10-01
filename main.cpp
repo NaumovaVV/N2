99ba4ca0b6cef3f1c19fbb5712257fd4661f6e44
Naumova <vikusya25.95@gmail.com>
2020-10-01 14:55:30 +0300

    i mistake Z

---
+++ main.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+ 
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+ 
+using namespace std;
+ 
+int main(int argc, char** argv) {
+ 
+int a, b, c;
+ 
+cout << "Input first number" << endl;
+cin >> a;
+ 
+cout << "Input second number" << endl;
+cin >> b;
+ 
+c = a + b;
+ 
+cout << "Sum " << c << endl;
+ 
+return 0;
+}
